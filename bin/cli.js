2b3faaae3e327826429a47bb88294c6a16299be6
Maik Schmidt <maik.schmidt.hl@gmail.com>
2018-12-07 11:34:10 +0100

    fix: fix package.json path

--- bin/cli.js
+++ bin/cli.js
@@ -3,7 +3,7 @@
 import program from 'commander';
 import cosmiconfig from 'cosmiconfig';
 
-import { authors, name, version } from '../package.json';
+import { authors, name, version } from '/package.json';
 import { build, generatePages, start, watch } from '../src/compossibru'; // eslint-disable-line
 
 const explorer = cosmiconfig('compossibru', { cache: false });
