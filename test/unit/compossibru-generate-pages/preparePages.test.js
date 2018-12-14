1171328a44d40fc38948ac3f3bfd77c6f736b5e2
Maik Schmidt <maik.schmidt.hl@gmail.com>
2018-12-14 11:21:11 +0100

    style: use ES6 syntax

--- test/unit/compossibru-generate-pages/preparePages.test.js
+++ test/unit/compossibru-generate-pages/preparePages.test.js
@@ -1,16 +1,20 @@
 const { preparePages } = require('../../../src/compossibru-generate-pages');
-const configurationWithoutWidgetContextInput = require('./mock/configuration_without_widget_context_input.json');
-const configurationWithoutWidgetContextOutput = require('./mock/configuration_without_widget_context_output.json');
-const configurationWithWidgetContextInput = require('./mock/configuration_with_widget_context_input.json');
-const configurationWithWidgetContextOutput = require('./mock/configuration_with_widget_context_output.json');
-const configurationMultipleRoutesWithoutWidgetContextInput = require('./mock/configuration_multiple_routes_without_widget_context_input.json');
-const configurationMultipleRoutesWithoutWidgetContextOutput = require('./mock/configuration_multiple_routes_without_widget_context_output.json');
-const configurationMultipleRoutesWithWidgetContextInput = require('./mock/configuration_multiple_routes_with_widget_context_input.json');
-const configurationMultipleRoutesWithWidgetContextOutput = require('./mock/configuration_multiple_routes_with_widget_context_output.json');
-const configurationWithSpecialCharWidgetInput = require('./mock/configuration_with_special_char_widget_input.json');
-const configurationWithSpecialCharWidgetOutput = require('./mock/configuration_with_special_char_widget_output.json');
-const configurationWithWidgetImportsInput = require('./mock/configuration_with_widget_imports_input.json');
-const configurationWithWidgetImportsOutput = require('./mock/configuration_with_widget_imports_output.json');
+import configurationWithoutWidgetContextInput from './mock/configuration_without_widget_context_input.json';
+import configurationWithoutWidgetContextOutput from './mock/configuration_without_widget_context_output.json';
+import configurationWithWidgetContextInput from './mock/configuration_with_widget_context_input.json';
+import configurationWithWidgetContextOutput from './mock/configuration_with_widget_context_output.json';
+import configurationMultipleRoutesWithoutWidgetContextInput
+    from './mock/configuration_multiple_routes_without_widget_context_input.json';
+import configurationMultipleRoutesWithoutWidgetContextOutput
+    from './mock/configuration_multiple_routes_without_widget_context_output.json';
+import configurationMultipleRoutesWithWidgetContextInput
+    from './mock/configuration_multiple_routes_with_widget_context_input.json';
+import configurationMultipleRoutesWithWidgetContextOutput
+    from './mock/configuration_multiple_routes_with_widget_context_output.json';
+import configurationWithSpecialCharWidgetInput from './mock/configuration_with_special_char_widget_input.json';
+import configurationWithSpecialCharWidgetOutput from './mock/configuration_with_special_char_widget_output.json';
+import configurationWithWidgetImportsInput from './mock/configuration_with_widget_imports_input.json';
+import configurationWithWidgetImportsOutput from './mock/configuration_with_widget_imports_output.json';
 
 test('compossibru-generate-pages: preparePages without widget context', () => {
     const pages = preparePages(
